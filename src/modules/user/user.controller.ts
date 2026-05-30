65f82f0f26fd689d58d983ab51dd3f4a14c9b9d3
subarnoturja <subarnoturja15@gmail.com>
2026-05-31 01:05:40 +0530

    controller for user login added

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -21,6 +21,24 @@ const createUser = async (req: Request, res: Response) => {
     }
 }
 
+const loginUser = async (req: Request, res: Response) => {
+    try {
+        const result = await userService.loginUserFromDB(req.body);
+
+        return sendResponse(res,StatusCodes.OK, {
+            success: true,
+            message: "Login successfully",
+            data: result,
+        })
+    } catch (error : any) {
+        return sendResponse(res,StatusCodes.INTERNAL_SERVER_ERROR, {
+            success: false,
+            message: error.message,
+        })
+    }
+}
+
 export const userController = {
-    createUser
+    createUser,
+    loginUser
 }
