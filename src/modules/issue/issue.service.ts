b21fcf50d6357bea44f479f432e50b7e54730ed2
subarnoturja <subarnoturja15@gmail.com>
2026-05-31 01:15:04 +0530

    query for get single issue added

--- src/modules/issue/issue.service.ts
+++ src/modules/issue/issue.service.ts
@@ -13,6 +13,29 @@ const createIssueIntoDB = async (payload: IIssue, reporterId: number) => {
     return result;
 }
 
+const getSingleIssueFromDB = async (id: string) => {
+    const issueResult = await pool.query(`
+            SELECT * FROM issues WHERE id=$1
+        `, [id]
+    )
+
+    const issue = issueResult.rows[0];
+
+    if(!issue) {
+        throw new Error("Issue Not Found");
+    }
+
+    const reporterResult = await pool.query(`
+            SELECT id, name, role FROM users WHERE id=$1
+        `, [issue.reporter_id]
+    )
+
+    const reporter = reporterResult.rows[0];
+
+    return { issue, reporter };
+}
+
 export const issueService = {
     createIssueIntoDB,
+    getSingleIssueFromDB,
 }
