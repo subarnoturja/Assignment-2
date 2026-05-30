011b3bd7ed854d66a951332a505026d9b775fd78
subarnoturja <subarnoturja15@gmail.com>
2026-05-31 01:23:37 +0530

    controller for create issue added

---
+++ src/modules/issue/issue.controller.ts
@@ -0,0 +1,29 @@
+import type { Request, Response } from "express";
+import { issueService } from "./issue.service";
+import sendResponse from "../../utils/sendResponse";
+import { StatusCodes } from "http-status-codes";
+
+// Create Issue
+const createIssue = async (req: Request, res: Response) => {
+    try {
+        const user = req.user as { 
+            id: number;
+        }
+        const result = await issueService.createIssueIntoDB(req.body, user.id);
+
+        return sendResponse(res, StatusCodes.CREATED, {
+            success: true,
+            message: "Issue Created Successfully!",
+            data: result.rows[0],
+        })
+    } catch (error: any) {
+        return sendResponse(res, StatusCodes.INTERNAL_SERVER_ERROR, {
+            success: false,
+            message: error.message,
+        })
+    }
+}
+
+export const issueController = {
+    createIssue,
+}
