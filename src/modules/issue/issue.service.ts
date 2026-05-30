7516896389c3da7ad70fdd389e6978890c0ac252
subarnoturja <subarnoturja15@gmail.com>
2026-05-31 01:18:06 +0530

    query for get all issues added

--- src/modules/issue/issue.service.ts
+++ src/modules/issue/issue.service.ts
@@ -13,6 +13,7 @@ const createIssueIntoDB = async (payload: IIssue, reporterId: number) => {
     return result;
 }
 
+// Get Single Issue From DB
 const getSingleIssueFromDB = async (id: string) => {
     const issueResult = await pool.query(`
             SELECT * FROM issues WHERE id=$1
@@ -35,7 +36,71 @@ const getSingleIssueFromDB = async (id: string) => {
     return { issue, reporter };
 }
 
+// Get All Issues From DB
+const getAllIssuesFromDB = async (sort = "newest", type?: string, status?: string) => {
+    const conditions : string[] = [];
+    const values: string[] = [];
+
+    if(type) {
+        values.push(type);
+        conditions.push(`type=$${values.length}`)
+    }
+
+    if(status) {
+        values.push(status);
+        conditions.push(`status = $${values.length}`);
+    }
+
+    let query = `SELECT * FROM issues`;
+
+    if(conditions.length > 0) {
+        query += ` WHERE ${conditions.join(' AND ')}`
+    }
+
+    query += `
+        ORDER BY created_at ${
+            sort === 'oldest' ? 'ASC' : 'DESC'
+        }
+    `
+
+    const result = await pool.query(query, values);
+
+    const issues = result.rows;
+
+    const reporterIds = [
+        ...new Set(
+            issues.map(issue => issue.reporter_id)
+        )
+    ]
+
+    const reporterQuery = `
+        SELECT id, name, role FROM users WHERE id=ANY($1)
+    `;
+
+    const reporterResult = await pool.query(reporterQuery, [reporterIds])
+
+    const reporters = reporterResult.rows
+
+    const finalIssues = issues.map(issue => ({
+    id: issue.id,
+    title: issue.title,
+    description: issue.description,
+    type: issue.type,
+    status: issue.status,
+    reporter:
+      reporters.find(
+        reporter => reporter.id === issue.reporter_id
+      ) || null,
+    created_at: issue.created_at,
+    updated_at: issue.updated_at,
+  }));
+
+  return finalIssues;
+
+}
+
 export const issueService = {
     createIssueIntoDB,
     getSingleIssueFromDB,
+    getAllIssuesFromDB,
 }
