1adefee5a1c2bcd3408fc3fc05de44fe375fc55f
subarnoturja <subarnoturja15@gmail.com>
2026-05-31 01:04:48 +0530

    controller for user signup are added

---
+++ src/modules/user/user.controller.ts
@@ -0,0 +1,26 @@
+import type { Request, Response } from "express";
+import { userService } from "./user.service";
+import sendResponse from "../../utils/sendResponse";
+import { StatusCodes } from "http-status-codes";
+
+const createUser = async (req: Request, res: Response) => {
+    try {
+        const result = await userService.createUserIntoDB(req.body);
+
+        return sendResponse(res, StatusCodes.CREATED, {
+            success: true,
+            message: "User registered successfully",
+            data: result.rows[0],
+        })
+
+    } catch (error : any) {
+        return sendResponse(res, StatusCodes.INTERNAL_SERVER_ERROR, {
+            success: false,
+            message: error.message,
+        })
+    }
+}
+
+export const userController = {
+    createUser
+}
